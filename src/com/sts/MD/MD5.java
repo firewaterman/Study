551ade7cbdf387ccff8ce323ec6ffa28a6ee0fb1
fireandwater <1271908900@qq.com>
2019-10-10 10:35:03 +0800

    第一次提交

---
+++ src/com/sts/MD/MD5.java
@@ -0,0 +1,46 @@
+package com.sts.MD;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @author ：gb
+ * @date ：Created in 2019/9/29 16:11
+ */
+public class MD5 {
+
+    public static void main(String[] args){
+        String s1 = stringMD("Aa123456");
+        System.out.println(s1);
+    }
+
+
+    public static String stringMD(String password){
+        if ("".equals(password.trim()) || password == null){
+            return "";
+        }
+
+        MessageDigest md5 = null;
+
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+
+            //转化成2进制
+            byte[] bytes = md5.digest(password.trim().getBytes());
+
+            String result = "";
+            for (byte by : bytes){
+                String temp = Integer.toHexString(by & 0xff);
+                if (temp.length() == 1){
+                    temp = "0" + temp;
+                }
+                result = result + temp;
+            }
+            return result;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+}
---
+++ src/com/sts/code/ImageCode.java
@@ -0,0 +1,34 @@
+package com.sts.code;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * @author ：gb
+ * @date ：Created in 2019/9/30 14:18
+ */
+public class ImageCode {
+
+    public BufferedImage image;
+    public String code;
+
+    public ImageCode(BufferedImage image, String code) {
+        this.image = image;
+        this.code = code;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
